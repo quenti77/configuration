8dc05d8ddbf5ffd218e28537d98f60c735faf9d5
quenti77 <quentin.ysambert@gmail.com>
2018-11-07 00:40:29 +0100

    i3 config

--- requires/i3.sh
+++ requires/i3.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+sudo pacman -S i3-gaps i3status i3lock
---
+++ requires/jetbrains.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Go to : https://www.jetbrains.com/toolbox/download/download-thanks.html"
+echo "Extract and run"
+
+sudo mv PATH_TO_TOOLBOX /usr/bin/
