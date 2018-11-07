706b3c40ba104750b4cfeab8312def08138aa20b
quenti77 <quentin.ysambert@gmail.com>
2018-11-07 22:48:49 +0100

    i3 + termite

--- requires/i3.sh
+++ requires/i3.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 sudo pacman -S i3-gaps i3status i3lock
+
+mkdir -p ~/.config/i3/
+cp ./../configs/i3/config ~/.config/i3/config
+
+mkdir -p ~/.config/rofi/
+cp ./../configs/i3/config.rasi ~/.config/rofi/config.rasi
+
+cp ./../configs/.Xresources ~/.Xresources
+xrdb ~/.Xresources
+
+pacman -S termite
+sudo cp ./../configs/termite/config /etc/xdg/termite/config
